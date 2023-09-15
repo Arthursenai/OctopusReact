e8ca4c40273eeb05435e2d2d22e45656e271fa2f
arthurborges <arthurborges.senai564@gmail.com>
2023-09-15 15:17:03 -0300

    feat: header test

---
+++ app/components/Header.jsx
@@ -0,0 +1,18 @@
+
+import styles from './header.module.css';
+import Image from 'next/image';
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.spacer}>
+            <Image className={styles.logo} src="/OctopusLogo.png" alt="Octopus" width={170} height={150} />
+            </div>
+            <div className={styles.p}>
+              <p className={styles.p} href="/">Home</p>
+              <p className={styles.p} href="/">About</p>
+              <p className={styles.p} href="/">Canhão</p>
+            </div>
+        </header>
+    );
+}
+export default Header;
--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,11 @@
-import styles from './page.module.css';
-import NavLink from './components/navlink/NavLink';
-import Image from 'next/image';
-export const Header = () => {
+import React from "react";
+import Header from "./components/Header";
+import style from "./page.module.css";
+
+export default function Home(){
     return (
-        <header className={styles.header}>
-            <h3 className={styles.title}>Octopus</h3>
-            <div className={styles.spacer}>
-            <Image className={styles.logo} src="/OctopusLogo.png" alt="Octopus" width={100} height={100} />
-            </div>
-            <div className={styles.links}>
-                <NavLink rota={'sa'} texto={'Example'} />
-                <NavLink rota={'sa'} texto={'example'} />
-                <NavLink rota={'sa'} texto={'Example'} />
-            </div>
-        </header>
+        <div className={style.bg}>
+          <Header />
+        </div>
     );
 }
